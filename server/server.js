5e5117e26a722004736543f71175d97cfcd93177
miyamonz <m5813k@gmail.com>
2022-08-29 20:11:30 +0900

    fix

--- server/server.js
+++ server/server.js
@@ -3,19 +3,11 @@ const morgan = require("morgan");
 
 const app = express();
 app.use(morgan("combined"));
+let i = 0;
 app.get("*", (_, response) => {
   response.setHeader("Content-Type", "application/json");
-  response.send(JSON.stringify({ msg: "hello world" }));
+  response.send(JSON.stringify({ msg: i++ }));
 });
 app.listen(3000, () => {
   console.log("start application server.");
-});
-
-const http2https = express();
-http2https.get("*", (request, response) => {
-  response.redirect(301, `https://${request.hostname}${request.url}`);
-});
-
-http2https.listen(3001, () => {
-  console.log("start redirect server.");
 });
