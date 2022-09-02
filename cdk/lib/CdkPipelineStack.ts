d7c44598729763359bfab52e2acedd73d784fd80
miyamonz <m5813k@gmail.com>
2022-09-02 16:00:28 +0900

    specify node version

--- cdk/lib/CdkPipelineStack.ts
+++ cdk/lib/CdkPipelineStack.ts
@@ -1,4 +1,5 @@
 import * as cdk from "aws-cdk-lib";
+import { BuildSpec } from "aws-cdk-lib/aws-codebuild";
 import {
   CodePipeline,
   CodePipelineSource,
@@ -6,23 +7,32 @@ import {
 } from "aws-cdk-lib/pipelines";
 import { Construct } from "constructs";
 
+const node16BuildSpec = BuildSpec.fromObject({
+  phases: {
+    install: {
+      "runtime-versions": {
+        nodejs: "16",
+      },
+    },
+  },
+});
+
 export class CdkPipelineStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
   }
+
   pipeline = new CodePipeline(this, "Pipeline", {
+    synthCodeBuildDefaults: {
+      partialBuildSpec: node16BuildSpec,
+    },
     synth: new ShellStep("Synth", {
       input: CodePipelineSource.connection("miyamonz/fargate-study", "main", {
         connectionArn:
           "arn:aws:codestar-connections:ap-northeast-1:843096071935:connection/47c4233d-0875-42ea-b13f-950c07a63fd6",
       }),
 
-      commands: [
-        "cd cdk",
-        "npm ci",
-        "npm run build",
-        "npm run cdk synth",
-      ],
+      commands: ["cd cdk", "npm ci", "npm run build", "npm run cdk synth"],
       primaryOutputDirectory: "cdk/cdk.out",
     }),
   });
